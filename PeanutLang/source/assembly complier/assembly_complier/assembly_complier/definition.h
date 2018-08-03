9131074ef44874c2f68028ccacbbb908f65cc764
AlexJason <cuizhihui030925@outlook.com>
2018-08-03 12:16:29 +0800

    AC:PUSH
    Add some function for append data.
    Statement 'push'
    For more in solution/Note/20180803.md

--- PeanutLang/source/assembly complier/assembly_complier/assembly_complier/definition.h
+++ PeanutLang/source/assembly complier/assembly_complier/assembly_complier/definition.h
@@ -15,9 +15,11 @@
 using byte = unsigned char;
 
 #ifdef X64
+#define PEANUT_64
 using PeanutInt = size_t;
 #define PI_MAX SIZE_MAX
 #else
+#define PEANUT_32
 using PeanutInt = size_t;
 #define PI_MAX SIZE_MAX
 #endif // X64
@@ -34,12 +36,12 @@ extern PeanutInt CS_Deep_Pos;//可执行代码区
 extern PeanutInt ES_Deep_Pos;//函数储存区
 
 //后面注释跟着的是参数,空白表示无参数,参数风格：类型+名称
-#define NUL 0x00//NULL
-#define PUSH_AX 0x01//NULL
-#define PUSH_BX 0x02//NULL
-#define PUSH_CX 0x03//NULL
-#define PUSH_ANY 0x04//PeanutInt address
-#define POP_AX 0x05//NULL
-#define POP_BX 0x06//NULL
-#define POP_CX 0x07//NULL
-#define POP_ANY 0x08//PeanutInt address
+#define NUL			0x00//NULL
+#define PUSH_AX		0x01//NULL
+#define PUSH_BX		0x02//NULL
+#define PUSH_CX		0x03//NULL
+#define PUSH_ANY	0x04//PeanutInt address
+#define POP_AX		0x05//NULL
+#define POP_BX		0x06//NULL
+#define POP_CX		0x07//NULL
+#define POP_ANY		0x08//PeanutInt address
--- PeanutLang/source/assembly complier/assembly_complier/assembly_complier/tools.h
+++ PeanutLang/source/assembly complier/assembly_complier/assembly_complier/tools.h
@@ -38,6 +38,27 @@ inline void AppendBin(std::string text, std::vector<byte> &Place) {
 	}
 }
 //************************************
+// Method:    AppendBinInt
+// FullName:  AppendBinInt
+// Access:    public 
+// Returns:   void
+// Qualifier: Append a int(32bit/64bit) to ByteCodes;
+// Parameter: PeanutInt n
+// Parameter: std::vector<byte> & Place
+//************************************
+inline void AppendBinInt(PeanutInt n, std::vector<byte> &Place) {
+#ifdef PEANUT_64
+	PeanutInt temp = 0xff00000000000000;
+	constexpr int bytes = 8;
+#else
+	PeanutInt temp = 0xff000000;
+	constexpr int bytes = 4;
+#endif
+	for (int i = bytes - 1; i >= 0; i--, temp >>= 8) {
+		Place.insert(Place.end(), static_cast<byte>((n & temp) >> i * 8));
+	}
+}
+//************************************
 // Method:    GetParameter
 // FullName:  GetParameter
 // Access:    public 
@@ -62,11 +83,24 @@ inline std::string GetParameter(std::string code, PeanutInt _off) {
 	}
 	return parameters.substr(last, next - last);
 }
-
+//************************************
+// Method:    GetOnlyString
+// FullName:  GetOnlyString
+// Access:    public 
+// Returns:   std::string
+// Qualifier: Get the string without head('#') and tail(';')
+// Parameter: std::string str
+//************************************
 inline std::string GetOnlyString(std::string str) {
 	return str.substr(1, str.size() - 2);
 }
-
+//************************************
+// Method:    GetPos_DS
+// FullName:  GetPos_DS
+// Access:    public 
+// Returns:   std::string
+// Qualifier: Get the size of DS
+//************************************
 inline PeanutInt GetPos_DS() {
 	if (DS.size() == 0) {
 		return 0;
@@ -74,5 +108,115 @@ inline PeanutInt GetPos_DS() {
 	else {
 		return DS.size() - 1;
 	}
-
 }
+//************************************
+// Method:    SetNumber32
+// FullName:  SetNumber32
+// Access:    public
+// Returns:   std::string
+// Qualifier: Set the number to 32 Bit
+// Parameter: std::string hex
+//************************************
+inline std::string SetNumber32(std::string hex) {
+	if (hex.size() == 8) {
+		return hex;
+	}
+	else if (hex.size() > 8) {
+		return hex.substr(0, 8);
+	}
+	else {
+		while (hex.size() < 8) {
+			hex.insert(hex.begin(), '0');
+		}
+		return hex;
+	}
+}
+//************************************
+// Method:    SetNumber64
+// FullName:  SetNumber64
+// Access:    public
+// Returns:   std::string
+// Qualifier: Set the number to 64 Bit
+// Parameter: std::string hex
+//************************************
+inline std::string SetNumber64(std::string hex) {
+	if (hex.size() == 16) {
+		return hex;
+	}
+	else if (hex.size() > 16) {
+		return hex.substr(0, 16);
+	}
+	else {
+		while (hex.size() < 16) {
+			hex.insert(hex.begin(), '0');
+		}
+		return hex;
+	}
+}
+
+#ifdef PEANUT_64
+#define SetNumberAuto SetNumber64
+#else
+#define SetNumberAuto SetNumber32
+#endif
+
+//************************************
+// Method:    IsNumberDec
+// FullName:  IsNumberDec
+// Access:    public
+// Returns:   bool
+// Qualifier: Check if argument is a number in tens
+// Parameter: std::string dec
+//************************************
+inline bool IsNumberDec(std::string dec) {
+	for (const auto &c : dec) {
+		if (c < '0' || c > '9') {
+			return false;
+		}
+	}
+	return true;
+}
+//************************************
+// Method:    IsNumberHex
+// FullName:  IsNumberHex
+// Access:    public
+// Returns:   bool
+// Qualifier: Check if argument is a number(HEX)
+// Parameter: std::string dec
+//************************************
+inline bool IsNumberHex(std::string hex) {
+	for (const auto &c : hex) {
+		if (!((c >= '0'&&c <= '9') || (c >= 'a'&&c <= 'f') || (c >= 'A'&&c <= 'F'))) {
+			return false;
+		}
+	}
+	return true;
+}
+//************************************
+// Method:    ToNumberFromHex
+// FullName:  ToNumberFromHex
+// Access:    public
+// Returns:   PeanutInt
+// Qualifier: Change string(hex number) to int(32/64 bit)
+// Parameter: std::string str
+//************************************
+inline PeanutInt ToNumberFromHex(std::string str) {
+	PeanutInt ret = 0x00;
+	long bitWeight = 1;
+	for (auto it = str.rbegin(); it != str.rend(); it++, bitWeight *= 16) {
+		if (*it >= '0' && *it <= '9') {
+			ret += (*it - 48) * bitWeight;
+		}
+		else if (*it >= 'a' && *it <= 'f') {
+			ret += (*it - 87) * bitWeight;
+		}
+		else if (*it >= 'A' && *it <= 'F') {
+			ret += (*it - 55) * bitWeight;
+		}
+		else {
+			break;
+		}
+	}
+	return ret;
+}
+
